66d67e43b177b7eec153d130f254dfb299f87bd4
Vilen <vilen_takiuti1@hotmail.com>
2025-07-01 02:59:37 -0300

    Adicionando o dia 5

---
+++ day5/quadrado_numero.py
@@ -0,0 +1,10 @@
+numero = int(input("coloque o numero desejado:"))
+
+def quadrado_numero(numero):
+    return numero ** 2
+
+def quadrado_numerov2(numero):
+    return numero * numero
+
+print(quadrado_numero(numero))
+print(quadrado_numerov2(numero))
