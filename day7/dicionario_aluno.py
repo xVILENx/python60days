fd19a4d92767300557cec6a056e43ab6ec151831
Vilen <vilen_takiuti1@hotmail.com>
2025-07-01 03:20:46 -0300

    Adicionando o dia 7

---
+++ day7/dicionario_aluno.py
@@ -0,0 +1,13 @@
+# dicionario em python
+
+aluno = {
+    "nome": "Vilen",
+    "idade": 20,
+    "curso": "Python 60 dias",
+    "matricula": 7394878828
+}
+
+print("Nome:", aluno["nome"])
+print("Idade:", aluno["idade"])
+print("Curso:", aluno["curso"])
+print("Matricula:", aluno["matricula"])
---
+++ day7/dicionario_dinamico.py
@@ -0,0 +1,8 @@
+aluno = {}
+
+aluno["nome"] = input("Digite o nome do aluno: ")
+aluno["idade"] = int(input("Digite a idade do aluno: "))
+aluno["curso"] = input("Digite o curso do aluno: ")
+aluno["matricula"] = int(input("Digite a matricula do aluno: "))
+
+print(aluno)
