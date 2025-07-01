f9b01ac1698f29a242c84393e1ff2556d99ab8ca
Vilen <vilen_takiuti1@hotmail.com>
2025-07-01 02:50:31 -0300

    Adicionando o dia 4

---
+++ day4/laco_de_repeticao.py
@@ -0,0 +1,10 @@
+# Utilizando o for para imprimir numeros de 0 a 10
+for numero in range(11):
+    print(numero)
+    
+#utilizando o while para imprimir numeros de 0 a 10
+
+numero = 0
+while numero < 11:
+    print(numero)
+    numero += 1
