acde5ffa7649269a471caeca0687fe7344ab1dce
Vilen <vilen_takiuti1@hotmail.com>
2025-07-01 02:22:05 -0300

    Adicionando o dia 2 de programacao

---
+++ day2/somar_dois_numeros.py
@@ -0,0 +1,6 @@
+numero1 = input("Digite o primeiro numero:")
+numero2 = input("Digite o segundo numero:")
+
+soma = int(numero1) + int(numero2)
+
+print(f"O resultado da soma é = {soma}")
---
+++ day2/somar_dois_numeros2.py
@@ -0,0 +1,16 @@
+def somar(numero1, numero2):
+    soma_total = numero1 + numero2
+    print(soma_total)
+    return soma_total
+
+#somar(1,4)
+
+def soma_dois_num():
+    numero1 = int(input("Digite o primeiro numero:"))
+    numero2 = int(input("Digite o segundo numero:"))
+    soma_total = numero1 + numero2
+    print(soma_total)
+    return soma_total
+
+soma_dois_num()
+    
