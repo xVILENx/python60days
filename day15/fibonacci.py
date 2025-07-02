777b8a95eea9acdfe7d338f44064f8f33e8c09e7
Vilen <vilen_takiuti1@hotmail.com>
2025-07-01 23:21:38 -0300

    Adicionando o dia 15

---
+++ day15/fibonacci.py
@@ -0,0 +1,8 @@
+fibonacci = [0, 1] # A sequência sempre começa com 0 e 1
+
+#só 8 pq seram só 10 numeros
+for i in range(8):
+    proximo_numero = fibonacci[-1] + fibonacci[-2]
+    fibonacci.append(proximo_numero)
+    
+print(fibonacci)
