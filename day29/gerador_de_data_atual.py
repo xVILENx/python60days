a8f7a63d0757ab5afa88c756b79b70454d3bd08f
Vilen <vilen_takiuti1@hotmail.com>
2025-07-08 23:36:52 -0300

    Adicionando o dia 29

---
+++ day29/gerador_de_data_atual.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytz
+
+
+def exibir_data_hora_atual():
+    """
+    Exibe data e hora atual do sistema
+    """  
+    
+    fuso_horario = pytz.timezone("America/Sao_Paulo")
+    
+    data_hora = datetime.now(fuso_horario)
+    
+    formato_data = data_hora.strftime("%d/%m/%y %H:%M:%S")
+    
+    print(f"Data e hora atual: {formato_data}")
+    
+if __name__ == "__main__":
+    exibir_data_hora_atual()
