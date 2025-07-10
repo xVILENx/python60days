ca4eb6b9a625c1cb5b6db8a7ce3010bc02689308
Vilen <vilen_takiuti1@hotmail.com>
2025-07-09 23:08:34 -0300

    Adicionando o dia 35

---
+++ day35/request_https.py
@@ -0,0 +1,15 @@
+import requests
+
+url = "https://api.chucknorris.io/jokes/random"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    print("Sucesso!")
+    data = response.json()
+    
+    print("Essa é a piada do Chuck Norris: ")
+    print(data['value'])
+else: 
+    print("Erro de chamar a API do Chuck Norris.")
+    
